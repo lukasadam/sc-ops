3acb4d2c1b831f65c777efd5c732de70e49f78b6
Lukas Adam <31369518+lukasadam@users.noreply.github.com>
2026-02-03 18:28:40 +0100

    updated adata ops

--- sc-ops/adata_ops.py
+++ sc-ops/adata_ops.py
@@ -3,7 +3,7 @@
 import anndata as ad
 import scanpy as sc
 import pandas as pd
-from _utils import group_by_max
+from _utils import group_by_max, minmax
 
 def aggregate(adata: ad.AnnData, groupby: str, method: str = 'mean') -> pd.DataFrame:
     """
@@ -23,7 +23,12 @@ def aggregate(adata: ad.AnnData, groupby: str, method: str = 'mean') -> pd.DataF
     AnnData
         The aggregated annotated data matrix.
     """
+    # Perform aggregation using scanpy's built-in function
     adata_agg = sc.get.aggregated(adata, groupby=groupby, method=method)
+    # Convert the aggregated AnnData to a DataFrame
     mat_agg = pd.DataFrame(adata_agg.X, index=adata_agg.obs_names, columns=adata_agg.var_names)
+    # Apply min-max scaling per gene
+    mat_agg = minmax(mat_agg, axis=0)
+    # Order DataFrame by group of max value
     mat_agg = group_by_max(mat_agg)
     return mat_agg
