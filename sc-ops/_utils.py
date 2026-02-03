ae383e7fcbcbb1a20de1b8379b3d3d394d310958
Lukas Adam <31369518+lukasadam@users.noreply.github.com>
2026-02-03 18:08:44 +0100

    added minmax

--- sc-ops/_utils.py
+++ sc-ops/_utils.py
@@ -18,3 +18,22 @@ def group_by_max(expr: pd.DataFrame) -> pd.DataFrame:
     for group in expr.columns:
         ordered.extend(expr[max_group == group].index.tolist())
     return expr.loc[ordered]
+
+def minmax(df: pd.DataFrame, axis=1, eps=1e-8) -> pd.DataFrame:
+    """Min-max normalize a DataFrame.
+
+    Parameters:
+    ----------
+    df : pd.DataFrame
+        Input DataFrame to be normalized.
+    axis : int
+        Axis along which to normalize (0 for columns, 1 for rows).
+    eps : float
+        Small value to avoid division by zero.
+
+    Returns:
+    -------
+    pd.DataFrame
+        Min-max normalized DataFrame.
+    """
+    return (df - df.min(axis=axis, keepdims=True)) / (df.max(axis=axis, keepdims=True) - df.min(axis=axis, keepdims=True) + eps)
